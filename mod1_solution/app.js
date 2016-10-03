04f5b56ed8c365bfba0e17e4c81e3ae2ce8042d8
Prasanna <pmategaonkar@gmail.com>
2016-10-03 23:46:55 +1100

    Final solution for module 1 assignment.

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,12 +1,25 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('LunchCheck',[])
-.controller('LunchCheckController',LunchCheckController);
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope){
-  $scope.message = "Too Much";
-
-}
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.input = '';
+        $scope.getLength = function () {
+            if ($scope.input.length != 0) {
+                var inputArr = $scope.input.split(',');
+                if (inputArr.length < 4) {
+                    $scope.message = 'Enjoy!';
+                }
+                else {
+                    $scope.message = 'Too much!';
+                }
+            }
+            else {
+                $scope.message = 'Please enter data first';
+            }
+        }
+    }
 })();
